dd65d4916ff15262898c1d5ffaa929424a4cd93d
MuikoGitahi <mgitahi040@gmail.com>
2025-03-30 14:22:35 +0300

    changed the code

--- WEEK-5/constructors.py
+++ WEEK-5/constructors.py
@@ -9,13 +9,20 @@
 #Example: Setting Up Your Constructor!
 
 class Car:
-    def __init__(self, color, model):
-        self.color = color  #An Instance Variable
-        self.model = model  #An Instance Variable
+    def __init__(self, color, model, top_speed):
+       self.color = color
+       self.model = model
+       self.top_speed = top_speed
+   
 
 #Creating 2 objects with unique attributes
-car1 = Car("Black", "Mercides")
-car2 = Car("Red", "Tesla")
+car1 = Car(color="Black", model="Mercedes", top_speed=220)
+car2 = Car(color="White",model="Dodge", top_speed=300)
 
+print(car1.model)
+print(car1.top_speed)
 print(car1.color)
 print(car2.model)
+print(car2.color)
+print(car2.top_speed)
+ 
